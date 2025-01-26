3f57709e622c4a97e819f11a58cce04ed72d0ccc
chaubui2001 <chaubui2001.job@gmail.com>
2025-01-26 21:02:59 +0700

    Initial commit
    Add Makefile, source files for building and running with static and shared libraries

---
+++ 01-general-knowledge/project/src/hellolinux.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "hello.h"
+
+void hellolinux(void) {
+    printf("Hello linux\n");
+}
---
+++ 01-general-knowledge/project/src/helloworld.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "hello.h"
+
+void helloworld(void) {
+    printf("Hello world\n");
+}
